d656792704f653461ec49e59d75a99a581cd4686
Alba <alba.v.lamas@gmail.com>
2017-03-01 18:19:52 +0100

    small changes

--- recommendation.py
+++ recommendation.py
@@ -14,13 +14,14 @@ alba.v.lamas@gmail.com
 class Recommendation(object):
 	recommendations = {"Rain":"Don't forget your umbrella",
 					 	"Chance of a Thunderstorm":\
-					 	"Today is not the day to hang clothes",
+					 	"Don't forget your raincoat!",
 					 	"Thunderstorm":\
-					 	"Stay at home, make popcorn and watch a good movie",
-					 	"Snow Showers":"It's cold, but it's funny and beauty ;)",
+					 	"Don't forget your raincoat!",
+					 	"Snow Showers":"Wear gloves, scarf, hat, anorak...",
 					 	"Clear":"Don't forget sun glasses and sunscreen",
+                        "Cloudy":"Leave your sungasses at home, otherwise tou look like a fool",
 				 	 	"Mostly Cloudy":"Leave your sungasses at home, otherwise tou look like a fool",
-					 	"Partly Cloudy":"Había alguna nuvecilla... pero ha escampao",
+					 	"Partly Cloudy":"Wear what you want, today is your day ;)",
 					 	"Chance of Rain":"Take your umbrella, just in case",
 					 	}
 	def recommend(self, condition):
